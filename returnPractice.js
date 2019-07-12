e0652c2cd189c1fd6b7c3dafb57e1c5f1f20c557
marcusmASC6 <21marcusm@lrei.org>
2019-07-12 09:47:52 -0400

    function with return

---
+++ returnPractice.js
@@ -0,0 +1,10 @@
+//if the return value is not specified the function returns undefined
+function someFunc(){
+    //return syntac
+return "Hello World";
+return 5;
+}
+
+const result = someFunc();
+
+console.log(result);
